726a7f4958adaa01fcc4295f2ff9a2f7810ce149
Quang Hung <voquanghung12tin@gmail.com>
2024-09-22 00:07:26 +0700

    updtae

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -2,13 +2,23 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World from spring boot!";
+        return "Hello World from spring boot! hihi";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "only user can access this";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "only admin can access this";
     }
 
 }
--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LaptopshopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LaptopshopApplication.class, args);
+    }
 
 }
